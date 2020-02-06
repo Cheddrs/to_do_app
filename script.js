235dc1c002b79e2c80cbc5f3f01e7006093b5a7c
Corey Hedberg <chedberg@gmail.com>
2020-02-05 21:35:13 -0500

    Added day, date, and time using JS.

--- script.js
+++ script.js
@@ -38,6 +38,35 @@ function showDate() {
   // Used for debugging. Delete when completed
   console.log(currentDay);
   console.log(currentDate);
+
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let timeOfDay;
+
+  if (hour < 12) {
+    timeOfDay = "AM";
+  } else {
+    timeOfDay = "PM";
+  }
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+
+  let currentTime = `${hour}:${minute} ${timeOfDay}`;
+
+  document.getElementById("displayTime").innerHTML = currentTime;
+
+  // Used for debugging. Delete when completed
+  console.log(currentTime);
 }
 
 showDate();
